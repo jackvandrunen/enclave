ffab08cb91fe961d0977fe5285c6d2c6380df833
fallingduck <jack@fallingduck.net>
2014-05-23 20:02:14 -0700

    Basic layout achieved

--- src/frontend.py
+++ src/frontend.py
@@ -21,30 +21,33 @@ class Main(wx.Frame):
         'Deals with setting up the GUI'
         panel = wx.Panel(self)
 
-        grid = wx.GridBagSizer(5, 5)
+        grid = wx.GridBagSizer(5, 10)
 
         friendlist = wx.ListBox(panel)
-        grid.Add(friendlist, (1, 1), span=(6, 2), flag=wx.EXPAND)
+        grid.Add(friendlist, (0, 0), span=(6, 2), flag=wx.EXPAND)
 
         button_ignore = wx.Button(panel, label="Ignore", size=(90, 28))
         button_add = wx.Button(panel, label="Add", size=(90, 28))
-        grid.Add(button_ignore, pos=(7, 1), flag=wx.BOTTOM)
-        grid.Add(button_add, pos=(7, 2))
+        grid.Add(button_ignore, pos=(6, 0), flag=wx.BOTTOM)
+        grid.Add(button_add, pos=(6, 1))
 
         message = wx.TextCtrl(panel)
-        grid.Add(message, pos=(7, 3), span=(1, 4), flag=wx.EXPAND | wx.RIGHT)
+        grid.Add(message, pos=(6, 2), span=(1, 4), flag=wx.EXPAND | wx.RIGHT)
 
         log = wx.TextCtrl(panel, style=wx.TE_MULTILINE | wx.TE_READONLY)
-        grid.Add(log, pos=(3, 3), span=(4, 4), flag=wx.EXPAND)
+        grid.Add(log, pos=(2, 2), span=(4, 4), flag=wx.EXPAND)
 
-        ipshow = wx.StaticText(panel, label='ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff')
-        grid.Add(ipshow, pos=(1, 3), span=(1, 4), flag=wx.EXPAND)
+        ipshow = wx.TextCtrl(panel, value='ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff', style=wx.TE_READONLY)
+        grid.Add(ipshow, pos=(0, 2), span=(1, 4), flag=wx.EXPAND)
 
         statusmenu = wx.ComboBox(panel, choices=['Available'], style=wx.CB_READONLY)
-        grid.Add(statusmenu, pos=(2, 3), span=(1, 1))
+        grid.Add(statusmenu, pos=(1, 2), span=(1, 1))
 
         statusmsg = wx.TextCtrl(panel)
-        grid.Add(statusmsg, pos=(2, 4), span=(1, 3), flag=wx.EXPAND)
+        grid.Add(statusmsg, pos=(1, 3), span=(1, 3), flag=wx.EXPAND)
+
+        grid.AddGrowableRow(2)
+        grid.AddGrowableCol(3)
 
         panel.SetSizerAndFit(grid)
 

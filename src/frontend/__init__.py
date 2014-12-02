9cf2d8460c5d132a76a239ebb51a90fa13e15623
Jack <jack@fallingduck.net>
2014-12-02 09:55:02 -0800

    More changes to the web server

--- src/frontend/__init__.py
+++ src/frontend/__init__.py
@@ -2,23 +2,21 @@ from stream420 import stream, launch
 from bottle import route, static_file, error
 
 
-@route('/')
-def main():
-    return static_file('main.html', root='./views', mimetype='text/html')
-
-
-@route('/<path:path>')
-def servef(path):
-    return static_file(path, root='./views')
-
-
-@error(500)
-@error(404)
-@error(401)
-def errormsg(err):
-    return 'Error!'
-
-
 def start(f2bq, b2fq, host='localhost', port=7775):
     out_stream, in_stream = stream('/stream', f2bq.put, b2fq)
+
+    @route('/')
+    def main():
+        return static_file('main.html', root='./views', mimetype='text/html')
+
+    @route('/<path:path>')
+    def servef(path):
+        return static_file(path, root='./views')
+
+    @error(500)
+    @error(404)
+    @error(401)
+    def errormsg(err):
+        return 'Error!'
+
     return launch(host=host, port=port)
